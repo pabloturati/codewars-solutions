024b9cc45259da86c63a5f7851b3c13073ea9560
pabloturati <pablo.turati@gmail.com>
2020-01-09 19:55:50 -0500

    Updated read me file and added new kata solution Title: Dollars and Cents

---
+++ Python/kata-8/kyu8-Dollars-and-Cents.py
@@ -0,0 +1,26 @@
+"""
+Title: Dollars and Cents
+Source: https://www.codewars.com/kata/55902c5eaa8069a5b4000083/train/python
+Language used: Python
+Level (kyu): 8
+
+Instructions:
+The company you work for has just been awarded a contract to build a payment gateway. In order to help move things along, you have volunteered to create a function that will take a float and return the amount formatting in dollars and cents.
+39.99 becomes $39.99
+The rest of your team will make sure that the argument is sanitized before being passed to your function although you will need to account for adding trailing zeros if they are missing (though you won't have to worry about a dangling period).
+Examples:
+3 needs to become $3.00
+3.1 needs to become $3.10
+Good luck! Your team knows they can count on you!
+"""
+
+# Solution
+
+
+def format_money(amount): return "${:.2f}".format(amount)
+
+# Test cases
+
+
+Test.assert_equals(format_money(39.99), '$39.99',
+                   "That's not formatted the way we expected")
--- README.md
+++ README.md
@@ -4,11 +4,15 @@ This is the GitHub repository where I save copies of the katas I've completed on
 
 ## About
 
-### What is codewars
+[Codewars](https://www.codewars.com/) is an educational community for computer programming. On the platform, software developers train on programming challenges known as Kata. These discrete programming exercises train a variety of skills in a variety of programming languages, and are completed within an online Integrated development environment.[source](https://en.wikipedia.org/wiki/Codewars)
 
-### Current honor Level
+### Profile badges and honor
+
+![logo](https://www.codewars.com/users/pabloturati/badges/large)
 
 ## List of Challenges
+
+//PENDING To be updated with completed kata and solutions and links to solutions.
 
 #### Javascript
 
